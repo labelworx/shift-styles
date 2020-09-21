9822e378eb888a8bab00db2c8321831cbad318d1
Chris Chambers <chris@label-worx.com>
2020-09-21 11:55:59 +0100

    First commit using Shift styles

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Labelworx;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+function styles(Finder $finder, array $rules = []): Config {
+    $rules = array_merge(require __DIR__.'/rules.php', $rules);
+
+    return Config::create()
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules($rules);
+}
